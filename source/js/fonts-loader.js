3b678c824d727dd90bf23243170a04f78084f680
lazypool <lazypool@proton.me>
2025-11-29 21:36:59 +0800

    feat: use cdn to load lxgw font-family

---
+++ source/js/fonts-loader.js
@@ -0,0 +1,13 @@
+(function (){
+	const jetBrainsFont = document.createElement("link");
+	jetBrainsFont.href = "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap";
+	jetBrainsFont.type = "text/css";
+	jetBrainsFont.rel = "stylesheet";
+	document.head.appendChild(jetBrainsFont);
+
+	const lxgwWenKaiFont = document.createElement("link");
+	lxgwWenKaiFont.href = "https://cdn.jsdelivr.net/npm/@callmebill/lxgw-wenkai-web@latest/style.css";
+	lxgwWenKaiFont.type = "text/css";
+	lxgwWenKaiFont.rel = "stylesheet";
+	document.head.appendChild(lxgwWenKaiFont);
+})();
